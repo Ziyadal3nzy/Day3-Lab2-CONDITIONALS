32b62656604d0abf90633d56c3e4c381dc26ee4c
Ziyad <ziyadal3nzy@gmail.com>
2022-08-02 13:53:27 +0300

    done

---
+++ p2.py
@@ -0,0 +1,9 @@
+My_movie = "Top Gun"
+rating = 3
+score = 72.65
+if rating >= 4 AND score > 80:
+    print("Higly recommended")
+elif rating >= 3 and score > 60:
+    print("i recommended it . it is good")
+elif rating <= 2 and score < 50:
+    print("Don't watch it. it is a waste of time")
